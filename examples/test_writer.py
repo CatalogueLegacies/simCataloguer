cdabdb66ccc526dd215b3a8befe507ded9a77ff5
James Baker <j.w.baker@soton.ac.uk>
2023-03-29 12:06:17 +0100

    Update test_writer.py

--- examples/test_writer.py
+++ examples/test_writer.py
@@ -1,8 +1,12 @@
 from simCataloguer import Writer
 
+outputdir = input("Write output folder name here: ")
 writer = Writer()
 writer(
     'MDG.txt',
     'simMDG',
     'outputs/hairout',
+    f'outputs/{outputdir}'
+    pixray_num_cuts=8,
+    pixray_quality="draft"
 )
